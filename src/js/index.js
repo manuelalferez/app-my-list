872344b06b4da84ece49e19e8979e267d93cfc3e
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-03-30 13:17:15 +0100

    Creada logo. Creada estetica de barra de busqueda

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const $buttonSearch = document.getElementById('button-search')
+const $iconSearch = document.getElementById('icon-search')
+
+debugger
+
+$buttonSearch.addEventListener('click', ()=>{
+    debugger
+    $iconSearch.classList.add('click')
+})
