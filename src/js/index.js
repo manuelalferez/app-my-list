a11e827610ac87e9c2e18f2ae9265807a57ef724
Manuel Alferez <manuelalferezruiz@gmail.com>
2019-03-30 19:13:45 +0100

    Creado boton para añadir tareas (sin funcionamiento). Creado la estetica de las tareas. Creado boton para eleminar tareas (sin funcionamiento)

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,3 @@
 const $buttonSearch = document.getElementById('button-search')
 const $iconSearch = document.getElementById('icon-search')
 
-debugger
-
-$buttonSearch.addEventListener('click', ()=>{
-    debugger
-    $iconSearch.classList.add('click')
-})
